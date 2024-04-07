6d5f90ea055a0dd3e2669de9b127305b5c499c6b
s.rostomyan <rostomyans@gmail.com>
2024-04-07 02:39:37 +0200

    CT-12: use profile from spring security config

--- src/main/java/com/example/chattest/ChatTestApplication.java
+++ src/main/java/com/example/chattest/ChatTestApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ChatTestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ChatTestApplication.class, args);
+
+		SpringApplication app = new SpringApplication(ChatTestApplication.class);
+		app.setAdditionalProfiles("app");
+		app.run(args);
 	}
 
 }
